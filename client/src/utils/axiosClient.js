35ed682cb0a6e6fd90f7066cebb2d34eee9fd8c0
Jayesh Jangle <jayeshjangle50@gmail.com>
2025-11-10 08:12:43 +0530

    url change

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://jeetcode-leetcode-clone-wcsw.onrender.com',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+  baseURL: "https://jeetcode-leetcode-clone-wcsw.onrender.com", // ✅ NEW URL
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosClient;
